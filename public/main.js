abc9ac66cc5be161baa24a9ef4fa4f41e983bef4
AndriyFediko <105150222+AndriyFediko@users.noreply.github.com>
2023-08-31 15:50:26 +0300

    add some js

--- public/main.js
+++ public/main.js
@@ -24,3 +24,102 @@ let myChart = new Chart(ctx, {
     type: 'pie',
     data: data
 });
+
+
+let i = 0;
+$("#signInForm").hide(0);
+$("#swichForm").click(() => {
+    i++;
+    if (i % 2 == 0) {
+        $("#swichForm").text("Sign up");
+        $("#registerForm").show(0);
+        $("#signInForm").hide(0);
+    } else {
+        $("#swichForm").text("Sign in");
+        $("#registerForm").hide(0);
+        $("#signInForm").show(0);
+    }
+});
+
+function generatePassword() {
+    let password = "";
+
+    const upperCase = "ABCDEFGHIGKLMNOPQRSTUVWXYZ";
+    const lowerCase = "absdefghigklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const symbols = "@#%^&()_+~|[]></-=";
+    const passwordLength = 12;
+
+    while(password.length < passwordLength){
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += numbers[Math.floor(Math.random() * numbers.length)];
+        password += symbols[Math.floor(Math.random() * symbols.length)];
+    }
+
+    return password;
+}
+
+function hidePassword(password){
+    let hidenPassword = "";
+    for(let i = 0; i<password.length; i++){
+        hidenPassword += "*";
+    }
+    return hidenPassword;
+}
+
+$("#generatePassword").click( () => {
+    $("#userPassword").val(generatePassword());
+});
+
+$("#generateServicePassword").click( () => {
+    $("#servicePassword").val(generatePassword());
+});
+
+$(".edit").click( () => {
+    $(".editCardPopUpBg").show(0);
+});
+
+$("#closeEditPage").click( () => {
+    $(".editCardPopUpBg").hide(0);
+});
+
+let servicesData = [{
+    serviceName: "YouTube",
+    servicePassword: "Tf0[Ah0%Ne2=",
+},
+];
+
+$(".visible").hide(0);
+
+$(".setVisibility").click( (e) => {
+    let closestCard = e.target.closest(".serviceCard");
+    let closestCardId = (closestCard.id).slice(1);
+    $(`#c${closestCardId} .servicePassword`).toggleClass("servicePasswordVisible");
+    if($(`#c${closestCardId} .servicePassword`).hasClass("servicePasswordVisible")){
+        $(`#c${closestCardId} .hiden`).hide(0);
+        $(`#c${closestCardId} .visible`).show(0);
+        $(`#c${closestCardId} .servicePassword`).text(servicesData[closestCardId].servicePassword);
+    } else{
+        $(`#c${closestCardId} .hiden`).show(0);
+        $(`#c${closestCardId} .visible`).hide(0);
+        $(`#c${closestCardId} .servicePassword`).text(hidePassword(servicesData[closestCardId].servicePassword));
+    }
+});
+
+new ClipboardJS('.copy');
+$(".copyAlert").hide(0);
+$(".copy").click( () => {
+    $(".copyAlert").slideDown(200);
+    setTimeout(()=>{
+        $(".copyAlert").slideUp(200);
+    }, 2000)
+})
+
+$("#registerForm").submit( (event) => {
+    event.preventDefault();
+});
+
+$("#signInForm").submit( (event) => {
+    event.preventDefault();
+});
--- server.js
+++ server.js
@@ -26,6 +26,9 @@ app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/register", (req, res)=>{
+    res.sendFile(path.join(__dirname, "public", "register.html"));
+});
 
 app.listen(PORT, () => {
     console.log("server works on port " + PORT);
